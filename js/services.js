17c6f446a2ade02e6a03ebddcf3da3171e3c4137
sativouf <sat@airnux.fr>
2016-03-16 16:49:58 +0100

    change QCM generation + add options

--- js/services.js
+++ js/services.js
@@ -9,7 +9,7 @@ angular.module('qcmffvl.services', [])
 
 .factory('API', function($http){
     return {
-        generateQCM: function(array, QCMID) {
+        generateQCM: function(array, options, QCMID) {
             var API = this;
             // we want to have 10 numbers tops to define the QCM ID,
             // so 2^33, which is 2^32 for the seed, and 1 bit to
@@ -23,10 +23,13 @@ angular.module('qcmffvl.services', [])
             // generate from a known QCM ID
             if (QCMID) {
                 // TODO: handle
-                if (API.verifyChecksumQCM(QCMID) == -1) {
+                if (API.verifyChecksum(QCMID) == -1) {
                     return -1;
                 }
-                QCMID = API.extractIDQCM(QCMID);
+                var uncomp = API.uncomputeID(QCMID)
+                QCMID = uncomp.num;
+                options = uncomp.options;
+
                 seed = QCMID % max32;
                 surseed = Math.floor(QCMID / max32);
                 // console.debug(seed);
@@ -60,7 +63,7 @@ angular.module('qcmffvl.services', [])
             }
             API.untickAnswers(array);
             // return QCM ID
-            return API.computeIDQCM(seed + max32 * surseed);
+            return API.computeID(seed + max32 * surseed, options);
         },
         tickAnswers: function(array) {
             var m = array.length;
@@ -80,16 +83,52 @@ angular.module('qcmffvl.services', [])
                 }
             }
         },
-        computeIDQCM: function(num) {
+        computeID: function(num, options) {
             var API = this;
-            return API.formatID(num, API.getChecksumQCM(num));
+            var padnum = API.pad(num, 10);
+            var optnum = API.pad(API.computeOptions(options),2);
+            var ck = API.pad(API.checksum(num, optnum),3);
+
+            return padnum + optnum + ck;
+        },
+        uncomputeID: function(ID) {
+            var API = this;
+            var num = parseInt(ID.substr(0,10),10);
+            var optnum = parseInt(ID.substr(10,2),10);
+            var opt = API.uncomputeOptions(optnum);
+            var ck = parseInt(ID.substr(12),10);
+            return { "ck":ck, "options":opt, "num":num, "optnum":optnum }
+        },
+        computeOptions: function(opt) {
+            var API = this;
+            // console.debug("-- computeOptions --");
+            // console.debug(opt);
+
+            // category : 2 options
+            // level : 3 options
+            // nbquestions : 5 options
+            // encode everything into a 2*3*5 number
+            var optnum = opt[0]*3*5 + opt[1]*5 + opt[2]+1;
+            return optnum;
+        },
+        uncomputeOptions: function(num) {
+            var opt = [];
+            var tmp;
+            opt[0] = Math.floor((num-1)/(3*5));
+            tmp = (num-1)%(3*5);
+            opt[1] = Math.floor(tmp / 5);
+            opt[2] = Math.floor(tmp % 5);
+            return opt;
         },
         // returns :
         // checksum if none given
         // -1 if the one given is bogus
-        checksum: function(num, nbits, ck) {
+        checksum: function(num, optnum, ck) {
+            // space 10^3 for parity bits : 2^9 < 10^3 < 2^10 : so 9 parity bits
+            var nbits = 9;
             var pbits = [];
-            var b2num = num.toString(2);
+            // num is on 10 digits, calculate the checksum for optnum + num (if they were strings)
+            var b2num = (num+optnum*Math.pow(10,10)).toString(2);
             var b2numlen = b2num.length;
             var step = Math.floor(b2numlen / nbits);
             var remaining = b2numlen % nbits;
@@ -112,43 +151,18 @@ angular.module('qcmffvl.services', [])
             }
             return csum;
         },
-        // nbits   : number of parity bits
-        verifyChecksum: function(ID, nbits, nbchars) {
+        verifyChecksum: function(ID) {
             var API = this;
-            var myid = API.unformatID(ID, nbchars);
-            // console.debug(myid);
-            return API.checksum(parseInt(myid.ID,10), nbits, parseInt(myid.ck,10));
-        },
-        getChecksumQCM: function(num) {
-            var API = this;
-            // we have a space of 100, we can fit 2^6 : 6 parity bits
-            return API.checksum(num, 6);
-        },
-        verifyChecksumQCM: function(ID) {
-            var API = this;
-            return API.verifyChecksum(ID, 6, 2);
+            if (ID.length != 15) {
+                return false;
+            }
+            var IDdict = API.uncomputeID(ID);
+            return (API.checksum(IDdict.num, IDdict.optnum, IDdict.ck) != -1);
         },
         pad: function(num, size) {
             return ('00000000000000000000000' + num).substr(-size);
         },
-        formatID: function(num, checksum) {
-            var API = this;
-            // prepend "0"s so that our number is of length 10
-            var str = API.pad(num, 10);
-            return API.pad(checksum,2) + str;
-        },
-        // nbchars : number of parity chars at the beginning of ID
-        unformatID: function(ID, nbchars) {
-            var ck = ID.substr(0,nbchars);
-            var num = ID.substr(nbchars);
-            return { ck: parseInt(ck),
-                     ID: parseInt(num) };
-        },
-        extractIDQCM: function(ID) {
-            var API = this;
-            var ret = API.unformatID(ID, 2);
-            return ret.ID;
-        }
+
         //TODO : move selftest here
     };
 });
