bfbdb56021b9c717eca6edecdae37799f3c1d7b9
sativouf <sat@airnux.fr>
2016-12-05 19:09:35 +0100

    Revert "do not erase answers when changing cat/level/nbq"
    This reverts commit 4eb88edf16d63d003b70fc5f2976e2643bb55312.

--- js/controllers.js
+++ js/controllers.js
@@ -233,6 +233,10 @@ angular.module('qcmffvl.controllers', [])
 		$timeout(function() {
 			$scope.main.displayLimit = 10000;
 		}, 0);
+        if ($scope.qcm && !$scope.main.exam.mode && $scope.main.checkAnswers) {
+            API.untickAnswers($scope.qcm);
+            $scope.main.checkAnswers = false;
+        }
     }
 
     $scope.collapseNav = function() {
