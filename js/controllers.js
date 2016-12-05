4eb88edf16d63d003b70fc5f2976e2643bb55312
sativouf <sat@airnux.fr>
2016-12-05 18:58:08 +0100

    do not erase answers when changing cat/level/nbq

--- js/controllers.js
+++ js/controllers.js
@@ -233,10 +233,6 @@ angular.module('qcmffvl.controllers', [])
 		$timeout(function() {
 			$scope.main.displayLimit = 10000;
 		}, 0);
-        if ($scope.qcm && !$scope.main.exam.mode && $scope.main.checkAnswers) {
-            API.untickAnswers($scope.qcm);
-            $scope.main.checkAnswers = false;
-        }
     }
 
     $scope.collapseNav = function() {
