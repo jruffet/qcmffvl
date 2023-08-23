7232e7a6a54cba345b8fc9d19503db23509358a5
Jérémy Ruffet <sat@airnux.fr>
2023-08-23 17:44:17 +0200

    fix(controller): properly load nbquestions opt from storage

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -210,7 +210,7 @@ angular.module('qcmffvl.controllers', [])
         var opt = [];
         opt[0] = $scope.main.sport.options.indexOf($scope.$storage.conf.sport);
         opt[1] = $scope.main.level.options.indexOf($scope.$storage.conf.level);
-        opt[2] = $scope.main.nbquestions.options.indexOf($scope.$storage.conf.nbquestions);
+        opt[2] = $scope.main.nbquestions.options.indexOf($scope.$storage.conf.nbquestions.toString());
         opt[3] = $scope.main.category.options.indexOf($scope.$storage.conf.category);
         return opt;
     }
