0434215609bd8fac169444f4439969cea9469d1e
Jérémy Ruffet <sat@airnux.fr>
2025-07-15 16:06:54 +0200

    chore: reformat

--- web/js/directives.js
+++ web/js/directives.js
@@ -4,32 +4,32 @@
 
 
 angular.module('qcmffvl.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
+  directive('appVersion', ['version', function (version) {
+    return function (scope, elm, attrs) {
       elm.text(version);
     };
   }])
-.directive('removeLoaderWhenReady', ['$timeout', function($timeout) {
-  return function(scope, element, attrs) {
-	    // angular.element(element).css('color','blue');
-	    if (scope.$last) {
-          $timeout(function() {
-            if (scope.$parent) {
-              scope.$parent.$parent.loading = false;
-            }
-          }, 1000);
-	    }
-  	}
-}])
-.directive('selectOnFocus', ['$window', function ($window) {
-  return {
-    restrict: 'A',
-    link: function (scope, element, attrs) {
-      element.on('focus', function () {
-        if (!$window.getSelection().toString()) {
-          this.setSelectionRange(0, this.value.length)
-        }
-      });
+  .directive('removeLoaderWhenReady', ['$timeout', function ($timeout) {
+    return function (scope, element, attrs) {
+      // angular.element(element).css('color','blue');
+      if (scope.$last) {
+        $timeout(function () {
+          if (scope.$parent) {
+            scope.$parent.$parent.loading = false;
+          }
+        }, 1000);
+      }
     }
-  };
-}]);
+  }])
+  .directive('selectOnFocus', ['$window', function ($window) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        element.on('focus', function () {
+          if (!$window.getSelection().toString()) {
+            this.setSelectionRange(0, this.value.length)
+          }
+        });
+      }
+    };
+  }]);
