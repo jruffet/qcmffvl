70852d0cc0a64ecc1ddafad5494701142d1a087c
Jérémy Ruffet <sat@airnux.fr>
2025-07-24 10:48:25 +0200

    feat: use parameter to fetch js+json
    done in order to prevent aggressive caching by the browser

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -170,14 +170,13 @@ angular.module('qcmffvl.controllers', [])
         $scope.loadJSON = function () {
             $scope.loading = true;
             $timeout(function () {
-                $http.get('./json/qcm_ffvl.json')
+                $http.get('./json/qcm_ffvl.json?v=' + $scope.qcmVersion)
                     .success(function (data, status, headers, config) {
                         $scope.main.qcmDate = data.date;
                         $scope.QCMFromJSON(data);
                     })
                     .error(function () {
                         var dlg = dialogs.error('Erreur', 'Impossible de charger le JSON');
-                        // dlg.result();
                     });
             }, 100);
         }
